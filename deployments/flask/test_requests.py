0a5b8aa973dba6ed7fe4db16ccba2e5d63ffc93e
XIN SUN <49686954+XIN-von-SUN@users.noreply.github.com>
2022-05-20 05:14:09 +0200

    empty main

--- deployments/flask/test_requests.py
+++
@@ -1,7 +0,0 @@
-import requests
-
-user_info = {'name': 'letian', 'age': 12, 'gender':'M'}
-r = requests.post("http://127.0.0.1:5000/test", data=user_info)
-
-print(f'response is:{r.text}')
-
--- deployments/post.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-url = 'http://fea3-145-38-196-223.ngrok.io/webhooks/rest/webhook'
-myobj = {"sender": "t11", "message": "hi"}
-
-x = requests.post(url, json=myobj)
-
-print(x.text)
